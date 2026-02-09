ac648dc22eeaa34e94c6545439484500fbcb6ccb
MAHAZAKA Fanomezantsoa Christinova <mahazakafanomezantsoa4@gmail.com>
2026-02-09 15:49:52 +0300

    cat et  objet

--- script.sql
+++ script.sql
@@ -6,5 +6,20 @@ USE message;
 CREATE TABLE IF NOT EXISTS users (
     id    INT AUTO_INCREMENT PRIMARY KEY,
     name  VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE
+    email VARCHAR(150) NOT NULL UNIQUE,
+
+);
+
+CREATE TABLE IF NOT EXISTS objets (
+    id_objet INT AUTO_INCREMENT PRIMARY KEY,
+    id_user  INT NOT NULL,
+    prix_estime DECIMAL(10, 2) NOT NULL,
+    id_categorie INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_categorie) REFERENCES categories(id)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL
 );
